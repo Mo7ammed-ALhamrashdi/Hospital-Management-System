e630fecd728a48abdb9baf4bbb5f222c770c43fb
MOHAMMED ALHAMRASHDI <Malhamrashdi800@gmail.com>
2026-08-19 10:38:50 +0400

    feat(entities): Person attributes + full constructor [Task 1.1] Edited

--- src/entities/Person.java
+++ src/entities/Person.java
@@ -1,44 +1,37 @@
 package entities;
 
-import org.w3c.dom.DOMStringList;
-
 import java.time.LocalDate;
+import java.util.Objects;
 
 public class Person {
-private String id;
-private String firstName;
-private String lastName;
-private LocalDate dateOfBirth;
-private String gender;
-private String phoneNumber;
-private String email;
-private String address;
-private String nationalId;
-private int age;
-private boolean active;
+    private String id;
+    private String firstName;
+    private String lastName;
+    private LocalDate dateOfBirth;
+    private String gender;
+    private String phoneNumber;
+    private String email;
+    private String address;
+    private String nationalId;
+    private int age;
+    private boolean activeStatus;
 
+    // Full constructor
     public Person(String id, String firstName, String lastName,
                   LocalDate dateOfBirth, String gender,
-                  String phoneNumber, String email,
-                  String address, String nationalId,
-                  int age, boolean active) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.dateOfBirth = dateOfBirth;
-        this.gender = gender;
-        this.phoneNumber = phoneNumber;
-        this.email = email;
-        this.address = address;
-        this.nationalId = nationalId;
-        this.age = age;
-        this.active = active;
-    }
-    //overloaded
-public Person(String id, String firstName, String List){
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-}
+                  String phoneNumber, String email, String address,
+                  String nationalId, int age, boolean activeStatus) {
 
+        setId(id);
+        setFirstName(firstName);
+        setLastName(lastName);
+        setDateOfBirth(dateOfBirth);
+        setGender(gender);
+        setPhoneNumber(phoneNumber);
+        setEmail(email);
+        setAddress(address);
+        setNationalId(nationalId);
+        setAge(age);
+        setActiveStatus(activeStatus);
+    }
 }
