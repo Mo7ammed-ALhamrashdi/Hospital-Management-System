fc60a5224d68e891a47a8239676fecceb1fcba26
MOHAMMED ALHAMRASHDI <Malhamrashdi800@gmail.com>
2026-08-19 12:30:09 +0400

    feat(entities): Patient extends Person + fields   [Task 1.2]

--- src/entities/Patient.java
+++ src/entities/Patient.java
@@ -1,4 +1,29 @@
 package entities;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 public class Patient {
+    private String bloodGroup;
+    private String emergencyContact;
+    private LocalDate registrationDate;
+    private ArrayList allergies;
+    private ArrayList recordIds;
+    private double outstandingBalance;
+    private boolean isInsured;
+
+    public Patient(String bloodGroup, String emergencyContact,
+                   LocalDate registrationDate, ArrayList allergies,
+                   ArrayList recordIds, double outstandingBalance,
+                   boolean isInsured) {
+        this.bloodGroup = bloodGroup;
+        this.emergencyContact = emergencyContact;
+        this.registrationDate = registrationDate;
+        this.allergies = allergies;
+        this.recordIds = recordIds;
+        this.outstandingBalance = outstandingBalance;
+        this.isInsured = isInsured;
+
+    }
 }
+
