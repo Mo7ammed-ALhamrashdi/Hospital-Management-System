d8327afd647fef0ca31c4489532094e2e35f1a4c
MOHAMMED ALHAMRASHDI <Malhamrashdi800@gmail.com>
2026-08-19 15:55:29 +0400

    feat(entities): Appointment class + fields   [Task 1.6]

--- src/entities/Appointment.java
+++ src/entities/Appointment.java
@@ -1,4 +1,94 @@
 package entities;
-
+import java.time.LocalDate;
+import java.time.LocalTime;
 public class Appointment {
+    private String appointmentId;
+    private String patientId;
+    private String doctorId;
+    private LocalDate appointmentDate;
+    private LocalTime appointmentTime;
+    private String status;
+    private String reason;
+    private boolean isFollowUp;
+    public Appointment(String appointmentId, String patientId,
+                       String doctorId, LocalDate appointmentDate,
+                       LocalTime appointmentTime, String status,
+                       String reason, boolean isFollowUp) {
+
+        this.appointmentId = appointmentId;
+        this.patientId = patientId;
+        this.doctorId = doctorId;
+        this.appointmentDate = appointmentDate;
+        this.appointmentTime = appointmentTime;
+        this.status = status;
+        this.reason = reason;
+        this.isFollowUp = isFollowUp;
+    }
+    //Getter
+
+    public String getAppointmentId() {
+        return appointmentId;
+    }
+
+    public String getPatientId() {
+        return patientId;
+    }
+
+    public String getDoctorId() {
+        return doctorId;
+    }
+
+    public LocalDate getAppointmentDate() {
+        return appointmentDate;
+    }
+
+    public LocalTime getAppointmentTime() {
+        return appointmentTime;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public boolean isFollowUp() {
+        return isFollowUp;
+    }
+    //Setter
+
+    public void setAppointmentId(String appointmentId) {
+
+        this.appointmentId = appointmentId;
+    }
+
+    public void setPatientId(String patientId) {
+        this.patientId = patientId;
+    }
+
+    public void setDoctorId(String doctorId) {
+        this.doctorId = doctorId;
+    }
+
+    public void setAppointmentDate(LocalDate appointmentDate) {
+        this.appointmentDate = appointmentDate;
+    }
+
+    public void setAppointmentTime(LocalTime appointmentTime) {
+        this.appointmentTime = appointmentTime;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public void setFollowUp(boolean followUp) {
+        isFollowUp = followUp;
+    }
 }
