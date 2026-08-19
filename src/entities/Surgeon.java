a8c400fa3b3feec5aab17a0638d24338db253e60
MOHAMMED ALHAMRASHDI <Malhamrashdi800@gmail.com>
2026-08-19 21:05:34 +0400

    feat(entities): Surgeon extends Doctor + fields   [Task 2.1]

--- src/entities/Surgeon.java
+++ src/entities/Surgeon.java
@@ -1,4 +1,31 @@
 package entities;
+import java.time.LocalDate;
+import java.util.ArrayList;
+    public class Surgeon extends Doctor {
 
-public class Surgeon {
+        private int surgeriesPerformed;
+        private boolean operationTheatreAccess;
+        private ArrayList upcomingSurgeryDates;
+
+        public Surgeon(String id, String firstName, String lastName,
+                       LocalDate dateOfBirth, String gender,
+                       String phoneNumber, String email, String address,
+                       String nationalId, int age, boolean activeStatus,
+                       String specialization, int experienceYears,
+                       double consultationFee, boolean isOnCall,
+                       int surgeriesPerformed,
+                       boolean operationTheatreAccess) {
+
+            // Person → Doctor → Surgeon
+            super(id, firstName, lastName,
+                    dateOfBirth, gender,
+                    phoneNumber, email, address,
+                    nationalId, age, activeStatus,
+                    specialization, experienceYears,
+                    consultationFee, isOnCall);
+
+            this.surgeriesPerformed = surgeriesPerformed;
+            this.operationTheatreAccess = operationTheatreAccess;
+            this.upcomingSurgeryDates = new ArrayList();
+        }
 }
