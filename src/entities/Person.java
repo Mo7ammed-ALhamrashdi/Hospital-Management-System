f09473b46dcc75ace5aec77a1d6b4b5d0563a542
MOHAMMED ALHAMRASHDI <Malhamrashdi800@gmail.com>
2026-08-19 11:23:50 +0400

    feat(entities): Person getters/setters [Task 1.1]

--- src/entities/Person.java
+++ src/entities/Person.java
@@ -40,5 +40,99 @@ public class Person {
         setFirstName(firstName);
         setLastName(lastName);
 }
+//Getter
 
+    public String getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getNationalId() {
+        return nationalId;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public boolean isActiveStatus() {
+        return activeStatus;
+    }
+
+//Setter
+    public void setId(String id) {
+        this.id = id;
+        if (id == null || id.trim().isEmpty()){
+
+        }
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setNationalId(String nationalId) {
+        this.nationalId = nationalId;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setActiveStatus(boolean activeStatus) {
+        this.activeStatus = activeStatus;
+    }
 }
+
+
