bbea69d447c492ca46abd43d6c3b9c7439fa5828
MOHAMMED ALHAMRASHDI <Malhamrashdi800@gmail.com>
2026-08-19 21:22:17 +0400

    feat(services): addPatient overloads (x3)   [Task 2.2]

--- src/services/PatientService.java
+++ src/services/PatientService.java
@@ -1,4 +1,19 @@
 package services;
 
+import entities.Patient;
+
 public class PatientService {
+
+    public void addPatient(String id, String name) {
+        System.out.println("Patient added: " + id + " " + name);
+    }
+
+    public void addPatient(String id, String name, String bloodGroup) {
+        System.out.println("Patient added: " + id + " " + name);
+        System.out.println("Blood Group: " + bloodGroup);
+    }
+
+    public void addPatient(Patient patient) {
+        System.out.println("Patient added: " + patient.getFullName());
+    }
 }
