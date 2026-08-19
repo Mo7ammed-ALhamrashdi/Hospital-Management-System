1e16fb8d0e6947d24a227b754b0c3e401c0f5cdf
MOHAMMED ALHAMRASHDI <Malhamrashdi800@gmail.com>
2026-08-19 21:29:52 +0400

    feat(services): schedule overloads (x3)   [Task 2.2]

--- src/services/AppointmentService.java
+++ src/services/AppointmentService.java
@@ -1,4 +1,34 @@
 package services;
 
+import entities.Appointment;
+import entities.Doctor;
+import entities.Patient;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class AppointmentService {
+
+    public void schedule(String patientId,
+                         String doctorId,
+                         LocalDate date) {
+
+        System.out.println("Appointment scheduled");
+    }
+
+    public void schedule(String patientId,
+                         String doctorId,
+                         LocalDate date,
+                         LocalTime time) {
+
+        System.out.println("Appointment scheduled");
+    }
+
+    public void schedule(Patient patient,
+                         Doctor doctor,
+                         LocalDate date,
+                         String reason) {
+
+        System.out.println("Appointment scheduled");
+    }
 }
