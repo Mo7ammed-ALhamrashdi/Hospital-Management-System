63c61c828dcd10d014385fa87f7a4e1fb5a441c5
MOHAMMED ALHAMRASHDI <Malhamrashdi800@gmail.com>
2026-08-20 10:37:33 +0400

    feat(utils): InputHandler text/number/decimal reads   [Task 2.6]

--- src/utils/InputHandler.java
+++ src/utils/InputHandler.java
@@ -1,4 +1,47 @@
 package utils;
-
+import java.util.Scanner;
 public class InputHandler {
+    private Scanner scanner = new Scanner(System.in);
+    public String readTaxt(String message) {
+    IO.println(message);
+        String text = scanner.nextLine();
+
+        while (!HelperUtils.isValidText(text)) {
+            IO.println("Invalid text. Try again:");
+            text = scanner.nextLine();
+        }
+
+        return text;
+    }
+
+    public int readNumber(String message) {
+        IO.println(message);
+
+        int number = scanner.nextInt();
+        scanner.nextLine();
+
+        while (!HelperUtils.isPositive(number)) {
+            IO.println("Number must be positive. Try again:");
+            number = scanner.nextInt();
+            scanner.nextLine();
+        }
+
+        return number;
+    }
+
+    public double readDecimal(String message) {
+        IO.println(message);
+
+        double number = scanner.nextDouble();
+        scanner.nextLine();
+
+        while (!HelperUtils.isPositive(number)) {
+            IO.println("Number must be positive. Try again:");
+            number = scanner.nextDouble();
+            scanner.nextLine();
+        }
+
+        return number;
+    }
 }
+
